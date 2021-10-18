afea3c2478f1a104434f1c7ff45dcf3c6806ca4e
Billions Joel <joelatugonza@gmail.com>
2021-10-18 14:38:18 +0300

    fixing hr scroll

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,10 +33,11 @@
     <header id="home">
         <div class="row header" id="home">
             <div class="col-sm-12">
-                <div class="center">
+                <div class="center" style="position: relative;">
                     <span class="heading-main">ProActive </span>
-                    <span class="heading-sub mb-4 border-bottom border-teal">wild Adventure.</span>
-                    <span class="heading-sub P-4">"Safe and Seemless Experience."</span></p>
+                    <span class="heading-sub mb-4 border-teal">wild Adventure.</span>
+                    <hr class="text-white w-25 center">
+                    <span class="heading-sub P-4 mt-4">"Safe and Seemless Experience."</span></p>
                     <a class="btn btn-light btn-lg" style="width:13rem; border-radius: 100px;" href="#services">Get
                         started</a>
                 </div>
